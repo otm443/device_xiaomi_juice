6755ed84c7247c158563fda5f4e266193df91480
Your Name <you@example.com>
2021-08-27 17:10:19 +0200

    Switch to lineage

--- lineage_juice.mk
+++ lineage_juice.mk
@@ -11,15 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_BLUR := true
-USE_PIXEL_CHARGING := true
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := lineage_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -30,6 +26,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 #Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
-
-#PixelBlaster
-BLASTER_BUILD_TYPE := OFFICIAL
